8cb0423dabcaeb7baf92f75e8c71266a6d0e1007
Zony Yu <zony249@gmail.com>
2025-11-17 16:16:18 -0700

    new task: alpaca_pp

--- simple-trainer/tasks/__init__.py
+++ simple-trainer/tasks/__init__.py
@@ -2,8 +2,11 @@
 from .wikitext import WikitextTask
 from .alpaca_plus import AlpacaPlus
 from .alpaca_plus.utils import DataCollatorForAlpacaCLM
+from .alpaca_pp import AlpacaPlusPlus
+from .alpaca_pp.utils import DataCollatorForAlpacaPlusPlusCLM
 
 TASK_MAP = {
     "wikitext": WikitextTask,
     "alpaca_plus": AlpacaPlus,
+    "alpaca_pp": AlpacaPlusPlus
 }
