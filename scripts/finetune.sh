d16b33c79870038d28866638f3530d2b4591d07b
Zony Yu <zony249@gmail.com>
2025-12-02 10:19:44 -0700

    switched from fsdp to ddp

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -4,30 +4,37 @@
 #SBATCH --cpus-per-task=8 
 #SBATCH --gpus-per-node=l40s:4
 #SBATCH --mem=400G
-#SBATCH --time=3-00:00
+#SBATCH --time=0-12:00
 #SBATCH --job-name=compression-llama7b
 #SBATCH --output=logs/%j--compression-llama7b--gist--higher-lr.out
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 export HF_HUB_OFFLINE=1
 export HF_HOME=$SCRATCH
-export TAG="gist--higher-lr"
+export TAG="gist"
 export DEBUGPY_ENABLE=0
+
+export NCCL_P2P_DISABLE=1
+
+# export NCCL_DEBUG=INFO
+# export NCCL_DEBUG_SUBSYS=ALL
+# export TORCH_DISTRIBUTED_DEBUG=INFO
+
+
 nvidia-smi 
 nvidia-smi topo -m
 
-# python -m debugpy --listen 0.0.0.0:5678 -m \
-    # accelerate.commands.launch \
-    # --config_file accel_config/fsdp2.yaml \
-# --config_file accel_config/fsdp2.yaml \
+mkdir -p $SCRATCH/runs
+
 accelerate launch \
-    --config_file accel_config/fsdp2.yaml \
+    --config_file accel_config/ddp.yaml \
     -m simple-trainer \
         --hf_name_or_path=llama-7b \
         --task=alpaca_plus \
-        --lr=2e-5 \
+        --lr=2e-4 \
         --epochs=4 \
-        --batch_size=4 \
-        --gradient_accumulation_steps=32 \
+        --batch_size=2 \
+        --gradient_accumulation_steps=10 \
         --eval_steps=2000 \
-        --output_dir=runs/$(date +%Y-%m-%d--%H-%M-%S)--$TAG \
+        --output_dir=$SCRATCH/runs/$(date +%Y-%m-%d--%H-%M-%S)--$TAG \
+        --lora_adapter=random_init \
--- scripts/finetune_no_gist.sh
+++ scripts/finetune_no_gist.sh
@@ -4,31 +4,33 @@
 #SBATCH --cpus-per-task=8 
 #SBATCH --gpus-per-node=l40s:4
 #SBATCH --mem=400G
-#SBATCH --time=3-00:00
+#SBATCH --time=0-12:00
 #SBATCH --job-name=llama7b
 #SBATCH --output=logs/%j--llama7b--no-gist--lower-lr.out
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 export HF_HUB_OFFLINE=1
 export HF_HOME=$SCRATCH
-export TAG="no-gist--lower-lr"
+export TAG="no-gist"
 export DEBUGPY_ENABLE=0
+
+export NCCL_P2P_DISABLE=1
+
 nvidia-smi 
 nvidia-smi topo -m
 
-# python -m debugpy --listen 0.0.0.0:5678 -m \
-    # accelerate.commands.launch \
-    # --config_file accel_config/fsdp2.yaml \
-# --config_file accel_config/fsdp2.yaml \
+mkdir -p $SCRATCH/runs
+
 accelerate launch \
-    --config_file accel_config/fsdp2.yaml \
+    --config_file accel_config/ddp.yaml \
     -m simple-trainer \
         --hf_name_or_path=llama-7b \
         --task=alpaca_plus \
-        --lr=5e-6 \
+        --lr=1e-4 \
         --epochs=4 \
-        --batch_size=4 \
-        --gradient_accumulation_steps=32 \
+        --batch_size=2 \
+        --gradient_accumulation_steps=10 \
         --eval_steps=2000 \
-        --output_dir=runs/$(date +%Y-%m-%d--%H-%M-%S)--$TAG \
+        --output_dir=$SCRATCH/runs/$(date +%Y-%m-%d--%H-%M-%S)--$TAG \
+        --lora_adapter=random_init \
         --turn_off_gist_masking \
